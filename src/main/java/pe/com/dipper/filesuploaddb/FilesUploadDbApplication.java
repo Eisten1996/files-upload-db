df110c018172c16e6655ce855bef62933cbefae5
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-09 17:43:32 -0500

    Initial commit

---
+++ src/main/java/pe/com/dipper/filesuploaddb/FilesUploadDbApplication.java
@@ -0,0 +1,13 @@
+package pe.com.dipper.filesuploaddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesUploadDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesUploadDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/dipper/filesuploaddb/FilesUploadDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.dipper.filesuploaddb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesUploadDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

68fe048c187b661aab88aa6ed0d1912a18185d05
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-09 19:55:37 -0500

    correciones en el aplication properties

--- src/main/java/pe/com/dipper/filesuploaddb/services/FileStorageService.java
+++ src/main/java/pe/com/dipper/filesuploaddb/services/FileStorageService.java
@@ -23,10 +23,12 @@ public class FileStorageService {
     }
 
     public FileDB getFile(String id) {
+
         return fileDBRepository.findById(id).get();
     }
 
     public Stream<FileDB> getAllFiles() {
+
         return fileDBRepository.findAll().stream();
     }
 }
